a2c03df340b2d618fcae691ae4910e11f8559955
Ishaan Jindal <ishaanjindal2006@gmail.com>
2025-11-30 17:45:10 +0530

    fold/unfold plugin neovim

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -144,4 +144,21 @@ return {
       require("nvim-surround").setup()
     end,
   },
+
+  -- nvim-ufo
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = { "kevinhwang91/promise-async" },
+    config = function()
+      vim.o.foldcolumn = "1"
+      vim.o.foldlevel = 99
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+
+      vim.o.foldmethod = "expr"
+      vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+      require("ufo").setup()
+    end
+  },
 }

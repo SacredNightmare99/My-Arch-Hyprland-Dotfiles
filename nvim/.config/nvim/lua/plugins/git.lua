edb70acac4255d602fa736ff944067b3c23c27f8
Ishaan Jindal <ishaanjindal2006@gmail.com>
2026-08-17 12:50:41 +0530

    enabled gitsigns current line blame
    Signed-off-by: Ishaan Jindal <ishaanjindal2006@gmail.com>

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -4,7 +4,22 @@ return {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPost", "BufNewFile" },
     config = function()
-      require("gitsigns").setup()
+      require("gitsigns").setup({
+        current_line_blame = true,
+
+        current_line_blame_opts = {
+          virt_text = true,
+          virt_text_pos = "eol",
+          delay = 500,
+        },
+
+        current_line_blame_formatter = "<author> • <author_time:%R> • <summary>",
+      })
+
+      vim.api.nvim_set_hl(0, "GitSignsCurrentLineBlame", {
+        fg = "#808080",
+        italic = true,
+      })
     end,
   },
 
